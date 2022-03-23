3b60b5fa1951fc2554d70c69227d44b8fd78424b
fjw3214 <2487429967@qq.com>
2022-03-23 19:42:32 +0800

    1

--- python/1.py
+++ python/1.py
@@ -1,13 +1,17 @@
-
+"""
+装饰器
+"""
 def fundemo(x):
     def fun1(y):
         ret=x(y)
         print("ret:",ret)
         return ret
+    print(fun1)
     return fun1
+
 @fundemo
 def fun2(x):
     return(x*222)
-# print(fun2)
+print(fun2)
 print(fun2(2))
 
